d122bf7a768d3e2802b9a2762fbc97d82b81c201
vaishudesh2025 <vaishudesh2007@gmail.com>
2026-05-13 13:48:38 +0530

    Create enen numbers.py

---
+++ enen numbers.py
@@ -0,0 +1,10 @@
+# Program to filter even numbers using filter()
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+
+print("Even Numbers:", even_numbers)
+
+Output:
+
+Even Numbers: [2, 4, 6, 8]
